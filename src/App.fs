2ae3e234ba201606d0eaf8f35ef0bd52764bacea
Yuan Jian <imyuanjian@gmail.com>
2022-12-19 23:58:29 +0800

    using minimist to parse args and a bunch of refactoring

--- src/App.fs
+++ src/App.fs
@@ -1,6 +1,7 @@
 module App
 
 open Fable.Core
+open Fable.Core.JsInterop
 open Octokit
 open SimpleFs
 open System.Text.RegularExpressions
@@ -13,16 +14,28 @@ let rec GITHUB_TOKEN =
 
 let octokit = new Octokit({ auth = GITHUB_TOKEN })
 
-type Input =
-    | UrlInput of string
-    | FilePathInput of string
-
 type ValidatedUrl = ValidatedUrl of string
 
+type Preferences = { AutoRemove: bool; SleepSeconds: int }
+
+type Input =
+    | HelpInput
+    | ErrorInput of string
+    | UrlInput of string * Preferences
+    | FilePathInput of string * Preferences
+
 let usage =
-    $"Usage: 
-    utube-go -u url
-    utube-go -p path (where path is path to a text file with each line containing a url)
+    $"Usage: utube-go [options]
+    
+    Cli utility that helps send urls to utube-download
+    
+    Options:
+    -u, --url [url]             Download single url
+    -p, --path [file_path]      Download all urls specified in a text file with each url in a separate line
+    
+    Additional preferences:
+    --auto-remove               Remove the issue after download
+    --sleep [n]                 Specifies the time to sleep in seconds after sending each url
 "
 
 let printAndExit (exitNo: int) (s: string) =
@@ -30,47 +43,61 @@ let printAndExit (exitNo: int) (s: string) =
     Node.Api.``process``.exit (exitNo)
     failwithf "will never reach here"
 
-let input =
-    Utils.getArgs ()
-    |> function
-        | [| "-u"; u |]
-        | [| "--url"; u |] -> UrlInput u
-        | [| "-p"; p |]
-        | [| "--path"; p |] -> FilePathInput p
-        | [| "-h" |]
-        | [| "--help" |] -> printAndExit 0 usage
-        | _ -> printAndExit 1 usage
+let validateUrl =
+    let urlPatt = new Regex(@"^http(s?)://")
 
-let urlPatt = new Regex(@"^http(s?)://")
+    let validator (u: string) : ValidatedUrl =
+        if urlPatt.IsMatch(u) then
+            ValidatedUrl u
+        else
+            failwithf "%s is not of valid url format" u
 
-let validateUrl u : ValidatedUrl =
-    if urlPatt.IsMatch(u) then
-        ValidatedUrl u
-    else
-        failwithf "%s is not of valid url format" u
+    validator
+
+let parseArgAsInput () : Input =
+    let opts: Minimist.Opts = createEmpty<Minimist.Opts>
+    opts.boolean <- Some !^[| "help"; "auto-remove" |]
+    opts.string <- Some !^[| "url"; "path"; "sleep" |]
+    let aliasOpt = createEmpty<Minimist.OptsAlias>
+    aliasOpt.Item("help") <- !^ "h"
+    aliasOpt.Item("url") <- !^ "u"
+    aliasOpt.Item("path") <- !^ "p"
+    opts.alias <- Some aliasOpt
+    let args = Minimist.minimist (Utils.getArgs ()) opts
+
+    let help =
+        if unbox<bool> (args.Item "help") then
+            Some HelpInput
+        else
+            None
+
+    let autoremove = unbox<bool option> (args.Item "auto-remove")
+    let sleep = unbox<string option> (args.Item "sleep")
+
+    let pref: Preferences =
+        { AutoRemove = autoremove |> Option.defaultValue false
+          SleepSeconds = sleep |> Option.map int |> Option.defaultValue 10 }
+
+    let url =
+        unbox<string option> (args.Item "url")
+        |> Option.map (fun u -> UrlInput(u, pref))
+
+    let path =
+        unbox<string option> (args.Item "path")
+        |> Option.map (fun p -> FilePathInput(p, pref))
+
+    [ help; url; path ]
+    |> Seq.choose id
+    |> Seq.tryHead
+    |> Option.defaultValue (ErrorInput "Please specify --url or --path")
 
 
-let urlsPromise: JS.Promise<ValidatedUrl list> =
-    let urls =
-        match input with
-        | UrlInput s -> Promise.lift ([ s ])
-        | FilePathInput f ->
-            promise {
-                let! content = SimpleFs.readFile (f, { encoding = Utf8 })
-
-                let lines =
-                    content.Split('\n')
-                    |> Seq.map (fun l -> l.Trim())
-                    |> Seq.filter (fun l -> l.Length > 0)
-                    |> Seq.toList
-
-                return lines
-            }
-
-    urls |> Promise.map (fun us -> us |> List.map validateUrl)
-
-let sendUrl (ValidatedUrl url) : JS.Promise<unit> =
-    let title = "http remove: " + (url.Split('=') |> Seq.last)
+let sendUrl (ValidatedUrl url) (secretive: bool) : JS.Promise<unit> =
+    let title =
+        if secretive then
+            "http remove: " + (url.Split('=') |> Seq.last)
+        else
+            url
 
     let issueReq: ICreateIssueRequest =
         { owner = "foresightyj"
@@ -87,24 +114,49 @@ let sendUrl (ValidatedUrl url) : JS.Promise<unit> =
             failwithf "Failed to create issue for url: %s with title: %s\n" url title
     }
 
-promise {
-    let! urls = urlsPromise
-    let total = urls |> List.length
-    JS.console.log ("totally", total)
+do
+    let input = parseArgAsInput ()
 
-    for (idx, validatedUrl) in urls |> Seq.indexed do
-        let idx = idx + 1
-        let (ValidatedUrl url) = validatedUrl
-        printf "Sending url %d/%d: %s\n" idx total url
-        // do! sendUrl validatedUrl
-        let isLast = (idx = total)
+    let (urlsPromise, pref) =
+        match input with
+        | HelpInput -> printAndExit 0 usage
+        | ErrorInput err -> printAndExit 1 (sprintf "%s\n\n%s" err usage)
+        | UrlInput(s, pref) -> (Promise.lift ([ s ]), pref)
+        | FilePathInput(p, pref) ->
+            (promise {
+                let! content = SimpleFs.readFile (p, { encoding = Utf8 })
 
-        if isLast then
-            let gotoUrl = "https://github.com/foresightyj/utube-download/actions"
-            printf "Done sending %d urls...\n\nAnd go %s to checkout results\n\n" total gotoUrl
-            return ()
-        else
-            JS.console.log ("Sleep for 10s...")
-            do! Promise.sleep (10000)
-}
-|> ignore
+                let lines =
+                    content.Split('\n')
+                    |> Seq.map (fun l -> l.Trim())
+                    |> Seq.filter (fun l -> l.Length > 0)
+                    |> Seq.toList
+
+                return lines
+             },
+             pref)
+
+    let validatedUrls =
+        urlsPromise |> Promise.map (fun us -> us |> List.map validateUrl)
+
+    promise {
+        let! urls = validatedUrls
+        let total = urls |> List.length
+        JS.console.log ("totally", total)
+
+        for (idx, validatedUrl) in urls |> Seq.indexed do
+            let idx = idx + 1
+            let (ValidatedUrl url) = validatedUrl
+            printf "Sending url %d/%d: %s" idx total url
+            do! sendUrl validatedUrl pref.AutoRemove
+            let isLast = (idx = total)
+
+            if isLast then
+                let gotoUrl = "https://github.com/foresightyj/utube-download/actions"
+                printf "Done sending %d urls...\n\nAnd go %s to checkout results\n\n" total gotoUrl
+                return ()
+            else
+                printf "Sleep for 10s..."
+                do! Promise.sleep (pref.SleepSeconds * 1000)
+    }
+    |> ignore
--- src/types/Minimist.fs
+++ src/types/Minimist.fs
@@ -1,58 +1,50 @@
 // ts2fable 0.7.1
-module rec Minimist
+module Minimist
 
 open System
 open Fable.Core
 open Fable.Core.JS
 
-[<Import("*", "minimist")>]
-let minimist: Minimist.IExports = jsNative
+[<AllowNullLiteral>]
+type ParsedArgs =
+    [<Emit "$0[$1]{{=$2}}">]
+    abstract Item: arg: string -> obj option with get, set
+
+    /// If opts['--'] is true, populated with everything after the --
+    abstract ``--``: string array option with get, set
+    /// Contains all the arguments that didn't have an option associated with them
+    abstract ``_``: string array with get, set
 
 [<AllowNullLiteral>]
-type IExports =
-    /// <summary>Return an argument object populated with the array arguments from args</summary>
-    /// <param name="args">An optional argument array (typically `process.argv.slice(2)`)</param>
-    /// <param name="opts">An optional options object to customize the parsing</param>
-    abstract minimist: ?args: ResizeArray<string> * ?opts: Minimist.Opts -> Minimist.ParsedArgs
+type OptsAlias =
+    [<Emit "$0[$1]{{=$2}}">]
+    abstract Item: key: string -> U2<string, string array> with get, set
 
-module Minimist =
+[<AllowNullLiteral>]
+type OptsDefault =
+    [<Emit "$0[$1]{{=$2}}">]
+    abstract Item: key: string -> obj option with get, set
 
-    [<AllowNullLiteral>]
-    type Opts =
-        /// A string or array of strings argument names to always treat as strings
-        abstract string: U2<string, ResizeArray<string>> option with get, set
-        /// A boolean, string or array of strings to always treat as booleans. If true will treat
-        /// all double hyphenated arguments without equals signs as boolean (e.g. affects `--foo`, not `-f` or `--foo=bar`)
-        abstract boolean: U3<bool, string, ResizeArray<string>> option with get, set
-        /// An object mapping string names to strings or arrays of string argument names to use as aliases
-        abstract alias: OptsAlias option with get, set
-        /// An object mapping string argument names to default values
-        abstract ``default``: OptsDefault option with get, set
-        /// When true, populate argv._ with everything after the first non-option
-        abstract stopEarly: bool option with get, set
-        /// A function which is invoked with a command line parameter not defined in the opts
-        /// configuration object. If the function returns false, the unknown option is not added to argv
-        abstract unknown: (string -> bool) option with get, set
-        /// When true, populate argv._ with everything before the -- and argv['--'] with everything after the --.
-        /// Note that with -- set, parsing for arguments still stops after the `--`.
-        abstract ``--``: bool option with get, set
+[<AllowNullLiteral>]
+type Opts =
+    /// A string or array of strings argument names to always treat as strings
+    abstract string: U2<string, string array> option with get, set
+    /// A boolean, string or array of strings to always treat as booleans. If true will treat
+    /// all double hyphenated arguments without equals signs as boolean (e.g. affects `--foo`, not `-f` or `--foo=bar`)
+    abstract boolean: U3<bool, string, string array> option with get, set
+    /// An object mapping string names to strings or arrays of string argument names to use as aliases
+    abstract alias: OptsAlias option with get, set
+    /// An object mapping string argument names to default values
+    abstract ``default``: OptsDefault option with get, set
+    /// When true, populate argv._ with everything after the first non-option
+    abstract stopEarly: bool option with get, set
+    /// A function which is invoked with a command line parameter not defined in the opts
+    /// configuration object. If the function returns false, the unknown option is not added to argv
+    abstract unknown: (string -> bool) option with get, set
+    /// When true, populate argv._ with everything before the -- and argv['--'] with everything after the --.
+    /// Note that with -- set, parsing for arguments still stops after the `--`.
+    abstract ``--``: bool option with get, set
 
-    [<AllowNullLiteral>]
-    type ParsedArgs =
-        [<Emit "$0[$1]{{=$2}}">]
-        abstract Item: arg: string -> obj option with get, set
 
-        /// If opts['--'] is true, populated with everything after the --
-        abstract ``--``: ResizeArray<string> option with get, set
-        /// Contains all the arguments that didn't have an option associated with them
-        abstract ``_``: ResizeArray<string> with get, set
-
-    [<AllowNullLiteral>]
-    type OptsAlias =
-        [<Emit "$0[$1]{{=$2}}">]
-        abstract Item: key: string -> U2<string, ResizeArray<string>> with get, set
-
-    [<AllowNullLiteral>]
-    type OptsDefault =
-        [<Emit "$0[$1]{{=$2}}">]
-        abstract Item: key: string -> obj option with get, set
+[<ImportDefault("minimist")>]
+let minimist (args: string array) (opts: Opts) : ParsedArgs = jsNative
